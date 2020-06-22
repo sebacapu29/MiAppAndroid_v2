4f318ada45bc5cdc2e422cb3a7f0a88f41d06323
sebacapu29 <sebacapu3@gmail.com>
2020-06-22 11:17:37 -0300

    Initial commit

---
+++ app/src/main/java/com/example/miappandroid_v2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.miappandroid_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        //Definir el evento Click sobre el boton de Login
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ComidasActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
